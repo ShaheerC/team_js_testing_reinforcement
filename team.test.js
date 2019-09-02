cf0617692841f63e590c97015598389b97ab85eb
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-09-01 23:19:22 -0400

    finished reinforcement

--- team.test.js
+++ team.test.js
@@ -1,5 +1,11 @@
 const teamObj = require('./team.js');
 
-// test('', () => {
-//
-// });
+test('giving a name and level argument initiates object with arguments', () => {
+    const result = new teamObj('Raptors', 10);
+
+    expect(result).toBeInstanceOf(Object);
+    expect(result.teamName).toEqual('Raptors');
+    expect(result.level).toEqual(10);
+    expect(result.points).toEqual(0);
+
+});
